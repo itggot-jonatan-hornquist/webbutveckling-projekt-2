7323fa472a35392c160758f4e9f23f29382c05d1
itggot-jonatan-hornquist <jonatan.hornquist@elev.ga.ntig.se>
2018-12-20 13:15:13 +0100

    Added a proper site folder structure, finished base HTML and CSS for mobile version. Started on basic JS functionality regarding popup.

---
+++ site/js/main.js
@@ -0,0 +1,8 @@
+function popUp(event){
+    let info = document.querySelector(".infobox")
+    info.classList.add("clicked")
+}
+
+function hide(event){
+    let parentNode = document.querySelector()
+}
