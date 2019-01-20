251ffb7754f664ce93de6d002a52b26e59eb2723
itggot-jonatan-hornquist <jonatan.hornquist@elev.ga.ntig.se>
2019-01-20 13:45:29 +0100

    Implemented easy image switching functionality in JS and created an desktop compatability framework

--- site/js/main.js
+++ site/js/main.js
@@ -1,8 +1,34 @@
 function popUp(event){
     let info = document.querySelector(".infobox")
+
+    let box_id = event.target.id
+
+    switch(box_id) {
+        case "box_1":
+            document.getElementById("info_img").src = "img/1.jpg"
+            document.getElementById("info_text").innerHTML = "Ett hjul är en cirkel-, torus- eller cylinderformad konstruktion för att underlätta en linjär rörelse, eller omvandla en linjär rörelse till rotationsrörelse.";
+            break;
+        case "box_2":
+            document.getElementById("info_img").src = "img/2.jpg"
+            document.getElementById("info_text").innerHTML = "hjul är mysiga"
+            break;
+        default:
+            document.getElementById("info_img").src = "img/oops.png"
+            document.getElementById("info_text").innerHTML = "något gick fel haha"
+            break;
+    }
+
     info.classList.add("clicked")
 }
 
 function hide(event){
-    let parentNode = document.querySelector()
+    let info = document.querySelector(".infobox")
+    info.classList.remove("clicked")
 }
+
+function initBox(box){
+    box.addEventListener("click", popUp )
+}
+
+let boxes = document.querySelectorAll(".box")
+boxes.forEach( initBox )
